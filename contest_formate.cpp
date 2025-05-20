9203dc7bf6c7cbe84f462262d0c08fd96a0b8bdd
Sazzad Hossain <0432220005101103@uits.edu.bd>
2025-05-19 20:45:11 -0700

    Update contest_formate.cpp

--- contest_formate.cpp
+++ contest_formate.cpp
@@ -3,8 +3,6 @@
 #define pb push_back
 #define maximum INT_MIN
 #define minimum INT_MAX
-#define fr for(int i=0; i<n; i++)
-#define frj for(int j=0; i<m; i++)
 #define vp vector<pair<int,int>>
 #define srt(v) sort(v.begin(),v.end())
 #define srt(s) sort(s.begin(),s.end())
@@ -42,126 +40,43 @@ int main()
 
 
 
-*************************String*************************
 
-    string s;
-    cin>>s;
-    getline(cin,s);// space soho input
-    cout<<s<<endl;
+
+
+*************************Number theory***********************
+
+***GCD LCM*****
+ll int gcd(ll int a, ll int b)
+{
+    if(b==0)return a;
+    return gcd(b,a%b);
+}
+ll int lcm(ll int a, ll int b)
+{
+    return a*b/gcd(a,b);
+}
+
+*******divisor*****
+    int n,cnt=0;
+    cin>>n;
+    set<int>s;
+    for(int i=1; i*i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            if(i%2!=0)s.insert(i);
+            if((n/i)%2!=0)s.insert(n/i);
+        }
+    }
+    cout<<"Total Odd divisor = "<<s.size()<<endl;
+    for(auto a : s)cout<<a<<" ";
+
     
-    string s="Sazad";
-    s.insert(s.begin()+2,1,'z'); // s.insert(s.begin()+index, jotobar, je char insert kobo)
-    cout<<s<<endl;
-
-    int len=s.size();
-    sort(s.begin(),s.end());
-    reverse(s.begin(),s.end());
-    s.erase(s.begin()+2);
-    s.pop_back();// ses r dik thika akta char delete kore
-    s.push_back('d');// ses r dike akta char add kore
-
-    string a;
-    a=s.substr(0,3);// suru and ses index
-
-    for(int i=0; i<s.size(); i++)
-    {
-        char c;
-        c=toupper(s[i]); //tolower(s[i]);
-        cout<<c;
-    }
-
-    istringstream stm(s); // akta string e joto gula word thake sob gula alada kore
-    string w;
-    while(stm>>w)cout<<w<<endl; //alada word gula print
-
-
-********************Number to string to number*****************
-
-int num=stoi(s);cout<<num<<endl;// use libaray function stirng to num
-    /*
-    int n=0;
-    for(int i=0; i<s.size(); i++)
-    {
-        int x=s[i]-'0';
-        n=(n*10)+x;
-    }
-    cout<<n<<endl;
-    */
-    int n2;cin>>n2;
-    string s1=to_string(n2); //use library function num to string
-    cout<<s1<<endl;
-    string s3=s1;
-    reverse(s3.begin(),s3.end());
-    if(s1==s3)cout<<"yes"<<endl;
-    else cout<<"no"<<endl;
-    /*
-    string s2;
-    while(n2>0)
-    {
-        int r=n2%10;
-        char c=r+'0';
-        s2+=c;
-        n2/=10;
-    }
-
-*************permutation********
-
-    string s;
-    cin>>s;
-    while(next_permutation(s.begin(),s.end()))
-    {
-        cout<<s<<endl;
-    }
-    vector<int>v={1,2,3,4};
-    while(next_permutation(v.begin(),v.end()))
-    {
-        for(int i=0; i<v.size(); i++)cout<<v[i]<<" ";
-        cout<<endl;
-    }
-    */
-    //pblm
-    string s;
-    cin>>s;
-    set<string>st;
-    sort(s.begin(),s.end());
-    st.insert(s);
-    while(next_permutation(s.begin(),s.end()))
-    {
-        st.insert(s);
-    }
-    cout<<st.size()<<endl;
-    for(auto a : st)cout<<a<<endl;
-
-
-
-********Sliding window******
-    
-    string s;cin>>s;
-    string t;cin>>t;
-    int a=0;
-    string c;
-    for(int i=0; i<t.size(); i++)
-    {
-        c+=s[i];
-    }
-    if(c==t)a++;
-    for(int i=t.size(); i<s.size(); i++)
-    {
-        c+=s[i];
-        c.erase(c.begin());
-        if(c==t)a++;
-    }
-    cout<<a<<endl;
-
-************************************************************************************************************
-
-
-****************Number theory*************
-
 ****2^n value****
     
     cout<<(1LL<<5)<<endl; // 2^n valu ber kore ( 1LL<<n )
     cout<<(32&31)<<endl;  // 2^n valu hobe ( n&(n-1) )=0 hoy;
+
 
 
 *****Sieve********
@@ -229,113 +144,384 @@ void sieve()
     if(f==1)cout<<"found  "<<"index = "<<idx<<endl;
     else cout<<"not found"<<endl;
 
+
+*********Binary Exponentiation**********
+
+long long int power(long long int base, long long int n,long long int mod) //time O(logn)
+{
+    long long result=1;
+    while(n)
+    {
+        if(n%2==1){result=(result*base)%mod; n--;}
+        else {base=(base*base)%mod; n/=2;}
+    }
+    return result;
+}
+
+*********coprime**********
+    
+ll int index[1002];
+ll int mark[1002];
+
+void coprime()
+{
+    ll n;
+    cin >> n;
+    map<ll,ll> mp;
+    for(int i = 0 ; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        mp[x] = i+1;
+    }
+    ll ans = -1;
+    for(auto i : mp)
+    {
+        for(auto j : mp)
+        {
+            if(__gcd(i.first,j.first) == 1)
+            {
+                ans = max(ans, (i.second+j.second));
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
+
+void coprime2()
+{
+    ll int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        index[x]=i+1;
+        mark[x]=1;
+    }
+    ll int ans=-1;
+    for(int i=1; i<=1000; i++)
+    {
+        for(int j=1; j<=1000; j++)
+        {
+            if(mark[i]==1 && mark[j]==1)
+            {
+                if(__gcd(i,j)==1)
+                {
+                    ans=max(ans,index[i]+index[j]);
+                }
+            }
+        }
+    }
+    cout<<ans<<"\n";
+}
+
+
+*************************************Algorithm*********************************
+
+*****coins change********
+    int coins[3]={1,2,3};
+    int change = 4;
+    int dp[change+1] ={0};
+    dp[0]=1;
+
+    for(int i=0; i<3;  i++){
+        for(int j=coins[i]; j<=change; j++){
+            dp[j]+=dp[j-coins[i]];
+        }
+    }
+    cout<<dp[change]<<endl;
+
+
+*******dfs*********
+cout<<"Enter number of nodes : ";
+    int n; cin>>n;
+
+    vector<int> graph[n];
+
+    cout<<"Enter edges : ";
+    int e; cin>>e;
+
+    for(int i=0; i<e; i++)
+    {
+        int nd1,nd2;
+        cin>>nd1>>nd2;
+        graph[nd1].push_back(nd2);
+        graph[nd2].push_back(nd1);
+    }
+    cout<<"Adjacency list :"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<i<<" : ";
+        for(int j=0; j<graph[i].size(); j++)
+        {
+            cout<<graph[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+//    DFS
+    stack<int>st;
+    int visited[n] = {0};
+    st.push(0);
+    while(!st.empty()){
+        int node = st.top();
+        if(visited[node]==1){
+            st.pop();
+            continue;
+        }
+        cout<<st.top()<<" ";
+
+        st.pop();
+        visited[node]=1;
+        for(int j=0; j<graph[node].size(); j++)
+        {
+            int adj_node = graph[node][j];
+            if(visited[adj_node]==0){
+                st.push(adj_node);
+            }
+        }
+    }
+    
+// 0/1 knapsak 
+    fast();
+    int t,cnt=1;
+    cin>>t;
+    while(t--){
+        int n;
+        int c;
+        cin>>n>>c;
+        vector<int>w,p;
+        int k[n+1][c+1];
+
+        for(int i=0; i<n; i++){
+            int a,b;
+            cin>>a>>b;
+            w.pb(a);
+            p.pb(b);
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=c; j++){
+                if(i==0 || j==0) k[i][j]=0;
+                else if(w[i]>j)k[i][j]=k[i-1][j];
+                else k[i][j]=max(k[i-1][j] ,k[i-1][j-w[i]] + p[i]);
+            }
+        }
+        cout<<"Case "<<cnt<<": "<<k[n-1][c]<<endl;
+        cnt++;
+
+    }
+
+
+
+//difference array
+
+
+void solve(){
+    int n; cin>>n;
+    vector<int>v,v1(n+1),v2;
+    for(int i=0; i<n; i++){
+        int a; cin>>a; v.pb(a);
+    }
+    int q; cin>>q;
+    for(int i=0; i<q; i++){
+        int l,r,va; cin>>l>>r>>va;
+        v1[l]+=va;
+        v1[r+1]-=va;
+    }
+    for(int i=1; i<n; i++)v1[i]+=v1[i-1];
+    for(int i=0; i<n; i++)v2.pb(v[i]+v1[i]);
+    for(auto a: v2)cout<<a<<" ";
+    cout<<endl;
+
+}
+
+
+
+//2D prefix sum
+
+
+void solve(){
+    int n,m; cin>>n>>m;
+    vector<vector<int>>v,v2(n,vector<int>(m));
+    for(int i=0; i<n; i++){
+        vector<int>t;
+        for(int j=0; j<m; j++){
+            int a; cin>>a; t.pb(a);
+        }
+        v.pb(t);
+    }
+
+    v2[0][0]=v[0][0];
+    for(int i=1; i<n; i++)v2[0][i]=v2[0][i-1]+v[0][i];
+    for(int i=1; i<m; i++)v2[i][0]=v2[i-1][0]+v[i][0];
+
+    for(int i=1; i<n; i++){
+        for(int j=1; j<m; j++){
+            v2[i][j]=v2[i][j-1]+v2[i-1][j]+v[i][j]-v2[i-1][j-1];
+        }
+    }
+    cout<<"prefix sum"<<endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<v2[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+
+*************************String*************************
+
+    string s;
+    cin>>s;
+    getline(cin,s);// space soho input
+    s.insert(s.begin()+2,1,'z'); // s.insert(s.begin()+index, jotobar, je char insert kobo)
+    s.size();
+    sort(s.begin(),s.end());
+    reverse(s.begin(),s.end());
+    s.erase(s.begin()+2);
+    s.pop_back();// ses r dik thika akta char delete kore
+    s.push_back('d');// ses r dike akta char add kore
+    c=toupper(s[i]); //tolower(s[i]);
+    a=s.substr(0,3);// suru and ses index
+
+    istringstream stm(s); // akta string e joto gula word thake sob gula alada kore
+    string w;
+    while(stm>>w)cout<<w<<endl; //alada word gula print
+
+
+********************Number to string to number*****************
+
+    int num=stoi(s);// use libaray function stirng to num
+    /*
+    int n=0;
+    for(int i=0; i<s.size(); i++)
+    {
+        int x=s[i]-'0';
+        n=(n*10)+x;
+    }
+    cout<<n<<endl;
+    */
+    int n2;cin>>n2;
+    string s1=to_string(n2); //use library function num to string
+    cout<<s1<<endl;
+    string s3=s1;
+    reverse(s3.begin(),s3.end());
+    if(s1==s3)cout<<"yes"<<endl;
+    else cout<<"no"<<endl;
+    
+    string s2;
+    while(n2>0)
+    {
+        int r=n2%10;
+        char c=r+'0';
+        s2+=c;
+        n2/=10;
+    }
+
+*************permutation********
+
+    string s;
+    cin>>s;
+    while(next_permutation(s.begin(),s.end()))
+    {
+        cout<<s<<endl;
+    }
+    vector<int>v={1,2,3,4};
+    while(next_permutation(v.begin(),v.end()))
+    {
+        for(int i=0; i<v.size(); i++)cout<<v[i]<<" ";
+        cout<<endl;
+    }
+    
+    //pblm
+    string s;
+    cin>>s;
+    set<string>st;
+    sort(s.begin(),s.end());
+    st.insert(s);
+    while(next_permutation(s.begin(),s.end()))
+    {
+        st.insert(s);
+    }
+    cout<<st.size()<<endl;
+    for(auto a : st)cout<<a<<endl;
+
+
+
+********Sliding window******
+    
+    string s;cin>>s;
+    string t;cin>>t;
+    int a=0;
+    string c;
+    for(int i=0; i<t.size(); i++)
+    {
+        c+=s[i];
+    }
+    if(c==t)a++;
+    for(int i=t.size(); i<s.size(); i++)
+    {
+        c+=s[i];
+        c.erase(c.begin());
+        if(c==t)a++;
+    }
+    cout<<a<<endl;
+
+************************************************************************************************************
+
+
+
+
 ****************************Vector************************
 
     vector<int> v; //vector declaration
     v.push_back(1);// value assin
-    v.push_back(20);
-    v.push_back(3);
-    v.push_back(4);
-
     vector<int> v1= { 10, 5, 8, 9, 6, 7, 3, 4, 2, 0 }; //vector declaration
-
-/* user input vector value
-    int n;
-    cin>>n;
-    for(int i=0; i<n(); i++)
-    {
-        int x;
-        cin>>x;
-        v.push_back(x);
-    }
-*/
-/*
-    cout<<v[0]<<endl;
-    cout<<v.at(0)<<endl;
-    cout<<v.size()<<endl;
-    for(int i=0; i<v.size(); i++)
-    {
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-
+    v.size();
     cout<<v.front()<<endl;//first value print
     cout<<v.back()<<endl;// last value print
-*/
 
 /// delete value:
-/*
-    //v.clear(); //vector all value delete kore
-    cout<<v.size()<<endl; // vector size
-
-    //v.pop_back(); //last value delete kore
-    //v.erase(v.begin()+0); // jekono index r value delete kore
+    v.clear(); //vector all value delete kore
+    v.pop_back(); //last value delete kore
+    v.erase(v.begin()+0); // jekono index r value delete kore
     v.erase(v.begin()+2,v.end()); // delete value (ak index theke onno index projonto)
-    for(int i=0; i<v.size(); i++)
-    {
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-
-    //v.empty(); // vector kali naki check kore
-*/
+    v.empty(); // vector kali naki check kore
 
 /// adding value:
-/*
-    //v.insert(v.begin(),1);// first index value add
-    //v.insert(v.begin()+1,1);// jekono index value add
+    v.insert(v.begin(),1);// first index value add
+    v.insert(v.begin()+1,1);// jekono index value add
     v.insert(v.begin()+2,3,1);// joto bar add korte chai
-    for(int i=0; i<v.size(); i++)
-    {
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-*/
 
 ///max valu vector: //*max_element (first_index, last_index);
-/*
     cout << "\n Max Element = "<< *max_element(a.begin(), a.end());
     cout << "\n Min Element = "<< *min_element(a.begin(), a.end());
-*/
+
 
 
 /// swaping two vector:
-/*
     swap(v,v1);
-    for(int i=0; i<v.size(); i++)
-    {
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-    for(int i=0; i<v1.size(); i++)
-    {
-        cout<<v1[i]<<" ";
-    }
-    cout<<endl;
-**/
 
 /// sort vector:
     sort(v.begin(),v.end());//ascending order
     sort(v.rbegin(),v.rend());//dacending
     sort(v.begin(), v.end(), greater <>());//dacending
     reverse(v.begin(),v.end());//dacending
-    for(int i=0; i<v.size(); i++)
-    {
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
 
 ///upper_bound
-   
     ll cnt=upper_bound(v.begin(),v.end(),m)-v.begin(); 
     //vector kono valu r last position  r next index print kore in vector
     cout<<cnt<<endl; // kono valu r last position/index jante  use korte pari
-    
+
     auto cnt=upper_bound(v.begin(),v.end(),m); // x thika boro valu ase kina 
     cout<<*cnt<<endl; // thakle boro valu  print kore , na thekle 0 print kore 
 
 ///lower_bound
-    
     ll cnt=lower_bound(v.begin(),v.end(),m)-v.begin();
     // fast position r ager index print kore
     cout<<cnt<<endl;
@@ -345,15 +531,9 @@ void sieve()
     cout<<*cnt<<endl;
     
 
-
-
 ///vector pair:
-    /*
     vector<pair<int,int>>m;
     m.push_back({2,1});
-    m.push_back({8,2});
-    m.push_back({3,3});
-    m.push_back({3,4});
 
     int arr[] = { 5, 20, 10, 40 };
     int arr1[] = { 30, 60, 20, 50 };
@@ -363,10 +543,9 @@ void sieve()
 
     for(auto a:m)cout<<a.first<<" "<<a.second<<endl;
 
-    */
 
 ///2D vector:
-/*
+
     vector<vector<int>> vect
     {
         {1, 2, 3},
@@ -393,11 +572,10 @@ void sieve()
         }
         cout << endl;
     }
-*/
 
 
 /// iteration:
-/*
+
     vector<int>::iterator it;
     //it=v.begin()+2;
     //cout<<*it<<endl;
@@ -406,7 +584,7 @@ void sieve()
         cout<<*it<<" ";
     }
     cout<<endl;
-*/
+
 
 
 
@@ -420,7 +598,6 @@ void sieve()
     //   key  , value
 
     mp.insert({1,5});
-    mp.insert({2,10});
     mp2.insert({"sazzad","you"});
     mp1[1]="sazzad"; // aivbe valu deya jay
     mp2["you"]="me";
@@ -444,9 +621,9 @@ void sieve()
     cout<<mp1.empty()<<endl;//yes hole 1 print korbe , No hole 0 print korbe
     cout<<mp2.empty()<<endl;
 
-    //mp.erase(1); // erase(key)
+    mp.erase(1); // erase(key)
     cout<<mp1.count(1)<<endl; //count(key) key ase naki setha khuje ber kore yes=1 no=0
-    //cout<<mp1.find(1)<<endl;
+    cout<<mp1.find(1)<<endl;
 
     auto b = mp.lower_bound(3); // key juthi thake tahole seti print korbe ,
                                 //na theke key thika boro jeti thakbe seti print,
@@ -462,9 +639,6 @@ void sieve()
 
     multimap<int,int>m4; // aki key onek bar thakte pare
     m4.insert({1,10});
-    m4.insert({2,20});
-    m4.insert({2,40});
-    m4.insert({1,50});
 
     auto it1 = m4.begin();
     advance(it1,2); // delet korbe first thika je index dlt korte chai
@@ -486,14 +660,11 @@ void sieve()
     cout<<n<<endl;
 
 ///sort:
-    //sort(arr, arr + n);
-    //sort(arr, arr + n, greater<>());//descending order
-    //sort(arr + 2, arr + n);// Sort the elements which lies in the range of 2 to (n-1)
+    sort(arr, arr + n);
+    sort(arr, arr + n, greater<>());//descending order
+    sort(arr + 2, arr + n);// Sort the elements which lies in the range of 2 to (n-1)
 
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    // Find the maximum element: *max_element (first_index, last_index);
+// Find the maximum element: *max_element (first_index, last_index);
     cout<<"\n Max Element = " << *max_element(arr, arr + n);
     cout<<"\n Max Element = " << *min_element(arr, arr + n)<<endl;
 
@@ -502,10 +673,10 @@ void sieve()
     array<int, 4> myarray1{ 1, 2, 3, 4 };
     array<int, 4> myarray2{ 3, 5, 7, 9 };
 
-        // using swap() function to swap elements of arrays
+// using swap() function to swap elements of arrays
     myarray1.swap(myarray2);
 
-        // printing the first array
+// printing the first array
     cout<<"myarray1 = ";
     for(auto it=myarray1.begin(); it<myarray1.end(); ++it)
         cout<<*it<<" ";
@@ -524,6 +695,7 @@ void sieve()
 
 
 
+*************************************Algorithm*********************************
 
 
 // 0/1 knapsak 
